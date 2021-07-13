1a0d604ebd3fd4e98dcd10d3c81015fa19eab9a6
Jishnudeep Bhattacharjee <bjishnudeep@gmail.com>
2021-07-13 12:38:11 +0530

    deployment

--- server/index.js
+++ server/index.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import bodyParser from 'body-parser';
+//import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
 import dotenv from 'dotenv';
@@ -8,8 +8,8 @@ import postRoutes from './routes/posts.js';
 
 const app = express();
 dotenv.config();
-app.use(bodyParser.json({ limit: '30mb', extended: true }))
-app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }))
+app.use(express.json({ limit: '30mb', extended: true }))
+app.use(express.urlencoded({ limit: '30mb', extended: true }))
 app.use(cors());
 
 app.use('/posts', postRoutes);
@@ -17,9 +17,9 @@ app.get('/', (req,res) => {
   res.send('Hello to Memories API');
 });
 
-const PORT = process.env.PORT|| 5000;
+const PORT = process.env.PORT || 5000;
 
-mongoose.connect(process.env.CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(process.env.CONNECTION, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => app.listen(PORT, () => console.log(`Server Running on Port: http://localhost:${PORT}`)))
   .catch((error) => console.log(`${error} did not connect`));
 
